690044e779dc8d782bf956f77103ae6a1edb2e2f
AjiPerdana38 <ajicooljazz38@gmail.com>
2022-01-04 17:33:35 +0700

    UAS Kelompok 5

---
+++ Rest_Api/Router/user_router.js
@@ -0,0 +1,20 @@
+/*
+Toko Buku
+M.Aji Perdana : 19411011
+Anny Hardiani : 19411074
+*/
+const router = require('express').Router()
+const userController = require('../Controller/user_controller')
+router.post('/sign-up', (req, res) => {
+  userController.register(req.body)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+router.post('/sign-in', (req, res) => {
+  userController.login(req.body)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+module.exports = router
